12cf60c3a1d87e75f955ef99bb8138d657cc75e5
liuxing <liuxinghy1987@126.com>
2019-01-08 17:53:06 +0800

    test

---
+++ liuxingTest/Tool1/Tool1.h
@@ -0,0 +1,17 @@
+//
+//  Tool1.h
+//  liuxingTest
+//
+//  Created by 跑的不快的小可爱 on 2019/1/8.
+//  Copyright © 2019年 跑的不快的小可爱. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tool1 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ liuxingTest/Tool1/Tool1.m
@@ -0,0 +1,13 @@
+//
+//  Tool1.m
+//  liuxingTest
+//
+//  Created by 跑的不快的小可爱 on 2019/1/8.
+//  Copyright © 2019年 跑的不快的小可爱. All rights reserved.
+//
+
+#import "Tool1.h"
+
+@implementation Tool1
+
+@end
